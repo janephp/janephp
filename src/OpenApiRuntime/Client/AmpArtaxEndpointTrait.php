d1ec123f01730c9c22f7d88b286719d28f1210b5
Frank Laszlo <flaszlo@nexcess.net>
2019-06-14 10:35:47 -0400

    Fix issue with incompatible function declaration

--- src/OpenApiRuntime/Client/AmpArtaxEndpointTrait.php
+++ src/OpenApiRuntime/Client/AmpArtaxEndpointTrait.php
@@ -12,13 +12,14 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 trait AmpArtaxEndpointTrait
 {
-    abstract protected function transformResponseBody(string $body, int $status, SerializerInterface $serializer);
+    abstract protected function transformResponseBody(string $body, int $status, SerializerInterface $serializer, ?string $contentType = null);
 
     public function parseArtaxResponse(Response $response, SerializerInterface $serializer, string $fetchMode = Client::FETCH_OBJECT): Promise
     {
         return call(function () use ($response, $serializer, $fetchMode) {
             if ($fetchMode === Client::FETCH_OBJECT) {
-                return $this->transformResponseBody(yield $response->getBody(), $response->getStatus(), $serializer);
+                $contentType = $response->hasHeader('Content-Type') ? current($response->getHeader('Content-Type')) : null;
+                return $this->transformResponseBody(yield $response->getBody(), $response->getStatus(), $serializer, $contentType);
             }
 
             if ($fetchMode === Client::FETCH_RESPONSE) {
