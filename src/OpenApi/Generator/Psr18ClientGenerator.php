5a3b102043b45b101c991d8b63d63e321d52f22e
Baptiste Leduc <baptiste.leduc@gmail.com>
2019-09-24 22:05:35 +0200

    Check if path exists before using it

--- src/OpenApi/Generator/Psr18ClientGenerator.php
+++ src/OpenApi/Generator/Psr18ClientGenerator.php
@@ -115,13 +115,13 @@ class Psr18ClientGenerator extends ClientGenerator
             $url = parse_url($server->getUrl());
             $baseUri = '';
 
-            if (isset($url['host'])) {
+            if (\array_key_exists('host', $url)) {
                 $scheme = $url['scheme'] ?? 'https';
                 $baseUri = $scheme . '://' . trim($url['host'], '/');
                 $plugins[] = AddHostPlugin::class;
             }
 
-            if (null !== $url['path']) {
+            if (\array_key_exists('path', $url) && null !== $url['path']) {
                 $baseUri .= '/' . trim($url['path'], '/');
                 $plugins[] = AddPathPlugin::class;
             }
