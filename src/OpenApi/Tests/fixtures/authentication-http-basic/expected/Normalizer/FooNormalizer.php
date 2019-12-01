c209b2236ef6c7eb167b248a889948ab3e41a76a
Baptiste Leduc <baptiste.leduc@gmail.com>
2019-12-01 16:43:10 +0100

    Fixing tests after rebase

--- src/OpenApi/Tests/fixtures/authentication-http-basic/expected/Normalizer/FooNormalizer.php
+++ src/OpenApi/Tests/fixtures/authentication-http-basic/expected/Normalizer/FooNormalizer.php
@@ -36,7 +36,9 @@ class FooNormalizer implements DenormalizerInterface, NormalizerInterface, Denor
     public function normalize($object, $format = null, array $context = array())
     {
         $data = new \stdClass();
-        $data->{'foo'} = $object->getFoo();
+        if (null !== $object->getFoo()) {
+            $data->{'foo'} = $object->getFoo();
+        }
         return $data;
     }
 }
