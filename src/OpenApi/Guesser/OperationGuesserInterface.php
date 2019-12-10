dd0396fea04583d2142e6174a03780d17b23472f
Baptiste Leduc <baptiste.leduc@gmail.com>
2019-12-10 22:33:44 +0100

    Cleaning & dependencies

--- src/OpenApi/Guesser/OperationGuesserInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Jane\OpenApi\Guesser;
-
-interface OperationGuesserInterface
-{
-    public function guessOperations();
-}
--- src/OpenApi2/Guesser/OperationGuesserInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Jane\OpenApi2\Guesser;
-
-interface OperationGuesserInterface
-{
-    public function guessOperations();
-}
