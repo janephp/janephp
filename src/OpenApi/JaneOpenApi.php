4a4a168734ba65780cf7eea783a34f7f5f1b0a15
Baptiste Leduc <baptiste.leduc@gmail.com>
2019-12-18 08:35:37 +0100

    Cleaning JaneOpenApi ~

--- src/OpenApi/JaneOpenApi.php
+++ src/OpenApi/JaneOpenApi.php
@@ -2,105 +2,19 @@
 
 namespace Jane\OpenApi;
 
-use Jane\JsonSchema\Generator\Context\Context;
 use Jane\OpenApi\Generator\AuthenticationGenerator;
 use Jane\OpenApi\Generator\ModelGenerator;
 use Jane\JsonSchema\Generator\Naming;
 use Jane\OpenApi\Generator\NormalizerGenerator;
-use Jane\JsonSchema\Guesser\ChainGuesser;
 use Jane\OpenApi\Generator\GeneratorFactory;
 use Jane\OpenApi\Guesser\OpenApiSchema\GuesserFactory;
 use Jane\OpenApi\SchemaParser\SchemaParser;
-use Jane\JsonSchema\Registry;
-use Jane\JsonSchema\Schema;
 use PhpParser\ParserFactory;
-use Symfony\Component\Serializer\Encoder\JsonDecode;
-use Symfony\Component\Serializer\Encoder\JsonEncode;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\YamlEncoder;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Yaml\Dumper;
-use Symfony\Component\Yaml\Parser;
 use Jane\OpenApiCommon\JaneOpenApi as CommonJaneOpenApi;
 
 class JaneOpenApi extends CommonJaneOpenApi
 {
-    public const VERSION = '5.x-dev';
-    public const CLIENT_PSR18 = 'psr18';
-    public const CLIENT_HTTPLUG = 'httplug';
-
-    private $schemaParser;
-
-    private $chainGuesser;
-
-    private $strict;
-
-    private $naming;
-
-    public function __construct(
-        SchemaParser $schemaParser,
-        ChainGuesser $chainGuesser,
-        Naming $naming,
-        bool $strict = true
-    ) {
-        $this->schemaParser = $schemaParser;
-        $this->chainGuesser = $chainGuesser;
-        $this->strict = $strict;
-        $this->naming = $naming;
-    }
-
-    public function createContext(Registry $registry): Context
-    {
-        $schemas = array_values($registry->getSchemas());
-
-        /** @var Schema $schema */
-        foreach ($schemas as $schema) {
-            $openApiSpec = $this->schemaParser->parseSchema($schema->getOrigin());
-            $this->chainGuesser->guessClass($openApiSpec, $schema->getRootName(), $schema->getOrigin() . '#', $registry);
-            $schema->setParsed($openApiSpec);
-        }
-
-        foreach ($registry->getSchemas() as $schema) {
-            foreach ($schema->getClasses() as $class) {
-                $properties = $this->chainGuesser->guessProperties($class->getObject(), $schema->getRootName(), $class->getReference(), $registry);
-                $names = [];
-
-                foreach ($properties as $property) {
-                    $property->setPhpName($this->naming->getPropertyName($property->getName()));
-
-                    $i = 2;
-                    $newName = $property->getPhpName();
-
-                    while (\in_array(strtolower($newName), $names, true)) {
-                        $newName = $property->getPhpName() . $i;
-                        ++$i;
-                    }
-
-                    if ($newName !== $property->getPhpName()) {
-                        $property->setPhpName($newName);
-                    }
-
-                    $names[] = strtolower($property->getPhpName());
-
-                    $property->setType($this->chainGuesser->guessType($property->getObject(), $property->getName(), $property->getReference(), $registry));
-                }
-
-                $class->setProperties($properties);
-
-                $extensionsTypes = [];
-
-                foreach ($class->getExtensionsObject() as $pattern => $extensionData) {
-                    $extensionsTypes[$pattern] = $this->chainGuesser->guessType($extensionData['object'], $class->getName(), $extensionData['reference'], $registry);
-                }
-
-                $class->setExtensionsType($extensionsTypes);
-            }
-
-            $this->hydrateDiscriminatedClasses($schema, $registry);
-        }
-
-        return new Context($registry, $this->strict);
-    }
+    protected const NORMALIZER_FACTORY_CLASS = JsonSchema\Normalizer\NormalizerFactory::class;
 
     public static function build(array $options = [])
     {
@@ -133,17 +47,5 @@ class JaneOpenApi extends CommonJaneOpenApi
         }
 
         return $self;
-    }
-
-    public static function buildSerializer()
-    {
-        $encoders = [
-            new JsonEncoder(new JsonEncode([JsonEncode::OPTIONS => JSON_UNESCAPED_SLASHES]), new JsonDecode()),
-            new YamlEncoder(new Dumper(), new Parser()),
-        ];
-
-        $normalizers = JsonSchema\Normalizer\NormalizerFactory::create();
-
-        return new Serializer($normalizers, $encoders);
     }
 }
