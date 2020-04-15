785b1c6c0b6a4ee9801c5539129c696414452aaf
Baptiste Leduc <baptiste.leduc@gmail.com>
2020-04-15 12:02:37 +0200

    Update Authentication plugin generation

--- src/OpenApi2/Generator/AuthenticationGenerator.php
+++ src/OpenApi2/Generator/AuthenticationGenerator.php
@@ -2,10 +2,10 @@
 
 namespace Jane\OpenApi2\Generator;
 
-use Jane\OpenApi2\Generator\Authentication\GetPluginGenerator;
+use Jane\OpenApi2\Generator\Authentication\HandleRequestGenerator;
 use Jane\OpenApiCommon\Generator\AuthenticationGenerator as CommonAuthenticationGenerator;
 
 class AuthenticationGenerator extends CommonAuthenticationGenerator
 {
-    use GetPluginGenerator;
+    use HandleRequestGenerator;
 }
--- src/OpenApi3/Generator/AuthenticationGenerator.php
+++ src/OpenApi3/Generator/AuthenticationGenerator.php
@@ -2,10 +2,10 @@
 
 namespace Jane\OpenApi3\Generator;
 
-use Jane\OpenApi3\Generator\Authentication\GetPluginGenerator;
+use Jane\OpenApi3\Generator\Authentication\HandleRequestGenerator;
 use Jane\OpenApiCommon\Generator\AuthenticationGenerator as CommonAuthenticationGenerator;
 
 class AuthenticationGenerator extends CommonAuthenticationGenerator
 {
-    use GetPluginGenerator;
+    use HandleRequestGenerator;
 }
