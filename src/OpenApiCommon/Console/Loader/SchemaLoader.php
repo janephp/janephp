1c9722287cbbe24207f6b02247543a2a1e1f8964
Filip Benčo <filip.benco@websupport.sk>
2020-06-01 14:41:55 +0200

    Fix CS

--- src/OpenApiCommon/Console/Loader/SchemaLoader.php
+++ src/OpenApiCommon/Console/Loader/SchemaLoader.php
@@ -31,7 +31,7 @@ class SchemaLoader extends BaseSchemaLoader implements SchemaLoaderInterface
             'skip-null-values',
             'version',
             'whitelisted-paths',
-            'endpoint-generator'
+            'endpoint-generator',
         ];
     }
 
