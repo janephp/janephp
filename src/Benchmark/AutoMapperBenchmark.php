102b652df6627e02d1a56078e9a54b60a2aa0897
Baptiste Leduc <baptiste.leduc@gmail.com>
2021-05-26 14:41:43 +0200

    Update benchmark

--- src/Benchmark/AutoMapperBenchmark.php
+++ src/Benchmark/AutoMapperBenchmark.php
@@ -2,42 +2,45 @@
 
 namespace Jane\Benchmark;
 
-use Jane\AutoMapper\AutoMapper;
-use Jane\AutoMapper\Compiler\Compiler;
-use Jane\AutoMapper\Compiler\FileLoader;
-use Jane\AutoMapper\Context;
-use Jane\AutoMapper\Tests\Domain\Address;
-use Jane\AutoMapper\Tests\Domain\User;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Jane\Component\AutoMapper\AutoMapper;
+use Jane\Component\AutoMapper\Generator\Generator;
+use Jane\Component\AutoMapper\Loader\FileLoader;
+use Jane\Component\AutoMapper\Tests\Fixtures\Address;
+use Jane\Component\AutoMapper\Tests\Fixtures\User;
 use PhpBench\Benchmark\Metadata\Annotations\BeforeMethods;
 use PhpBench\Benchmark\Metadata\Annotations\Iterations;
 use PhpBench\Benchmark\Metadata\Annotations\Revs;
 use PhpBench\Benchmark\Metadata\Annotations\Warmup;
+use PhpParser\ParserFactory;
+use Symfony\Component\Serializer\Mapping\ClassDiscriminatorFromClassMetadata;
+use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
+use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
 
 /**
  * @BeforeMethods({"initAutomapper"})
  * @Warmup(1)
- * @Revs(100000)
- * @Iterations(1)
+ * @Revs(60)
+ * @Iterations(60)
  */
 class AutoMapperBenchmark
 {
-    private $mapper;
-    private $context;
+    private $autoMapper;
 
     public function initAutomapper()
     {
-        $loader = new FileLoader(new Compiler(), __DIR__ . '/../../cache');
-        $autoMapper = AutoMapper::create(true, $loader);
+        @unlink(__DIR__ . '/../../cache');
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
 
-        $this->mapper = $autoMapper->getMapper(User::class, 'array');
+        $loader = new FileLoader(new Generator(
+            (new ParserFactory())->create(ParserFactory::PREFER_PHP7),
+            new ClassDiscriminatorFromClassMetadata($classMetadataFactory)
+        ), __DIR__ . '/../../cache');
+
+        $this->autoMapper = AutoMapper::create(true, $loader);
     }
 
-    public function initData()
-    {
-        $this->context = new Context();
-    }
-
-    public function benchToArray()
+    public function benchAutoMapper()
     {
         $address = new Address();
         $address->setCity('Toulon');
@@ -45,6 +48,6 @@ class AutoMapperBenchmark
         $user->address = $address;
         $user->addresses[] = $address;
 
-        $this->mapper->map($user, new Context());
+        $this->autoMapper->map($user, 'array');
     }
 }
