557699271760c5c75c994bbfa333fd6134cb51ab
Geoffrey Saleur <geoffrey.saleur@indricks.com>
2021-06-18 11:50:17 +0200

    fix php-cs-fixer

--- src/Component/AutoMapper/Generator/Generator.php
+++ src/Component/AutoMapper/Generator/Generator.php
@@ -311,7 +311,7 @@ final class Generator
 
         if (\stdClass::class === $target && \stdClass::class === $source) {
             return [[new Stmt\Expression(new Expr\Assign($result, new Expr\FuncCall(new Name('unserialize'), [new Arg(new Expr\FuncCall(new Name('serialize'), [new Arg($sourceInput)]))])))], [], [], []];
-        } else if (\stdClass::class === $target) {
+        } elseif (\stdClass::class === $target) {
             return [[new Stmt\Expression(new Expr\Assign($result, new Expr\New_(new Name(\stdClass::class))))], [], [], []];
         }
 
