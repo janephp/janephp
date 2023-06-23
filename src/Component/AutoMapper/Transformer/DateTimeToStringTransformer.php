b21b965e722e27408489f9912e022f59b2204010
Nicolas PHILIPPE <nikophil@gmail.com>
2023-06-23 10:05:04 +0200

    feat(AutoMapper): configure date format with context

--- src/Component/AutoMapper/Transformer/DateTimeToStringTransformer.php
+++ src/Component/AutoMapper/Transformer/DateTimeToStringTransformer.php
@@ -4,9 +4,10 @@ namespace Jane\Component\AutoMapper\Transformer;
 
 use Jane\Component\AutoMapper\Extractor\PropertyMapping;
 use Jane\Component\AutoMapper\Generator\UniqueVariableScope;
+use Jane\Component\AutoMapper\MapperContext;
 use PhpParser\Node\Arg;
 use PhpParser\Node\Expr;
-use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Scalar;
 
 /**
  * Transform a \DateTimeInterface object to a string.
@@ -28,7 +29,12 @@ final class DateTimeToStringTransformer implements TransformerInterface
     public function transform(Expr $input, Expr $target, PropertyMapping $propertyMapping, UniqueVariableScope $uniqueVariableScope): array
     {
         return [new Expr\MethodCall($input, 'format', [
-            new Arg(new String_($this->format)),
+            new Arg(
+                new Expr\BinaryOp\Coalesce(
+                    new Expr\ArrayDimFetch(new Expr\Variable('context'), new Scalar\String_(MapperContext::DATETIME_FORMAT)),
+                    new Scalar\String_($this->format),
+                )
+            ),
         ]), []];
     }
 }
