1cb93d61dd830dcac38298d8ee5783e75d33fd8d
Cedric Ziel <cedric@cedric-ziel.com>
2024-08-08 23:41:51 +0200

    fix: compare lowercase content types to create matches

--- src/Component/OpenApi3/Generator/Endpoint/GetTransformResponseBodyTrait.php
+++ src/Component/OpenApi3/Generator/Endpoint/GetTransformResponseBodyTrait.php
@@ -203,8 +203,12 @@ EOD
                 $statements[] = new Stmt\If_(
                     new Expr\BinaryOp\NotIdentical(
                         new Expr\FuncCall(new Name('mb_strpos'), [
-                            new Node\Arg(new Expr\Variable('contentType')),
-                            new Node\Arg(new Scalar\String_($contentType)),
+                            new Node\Arg(
+                                new Expr\FuncCall(new Name('strtolower'), [
+                                    new Expr\Variable('contentType'),
+                                ]),
+                            ),
+                            new Node\Arg(new Scalar\String_(strtolower($contentType))),
                         ]),
                         new Expr\ConstFetch(new Name('false'))
                     ),
