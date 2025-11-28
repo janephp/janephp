6ed01e2714c3b048665b5ede5c23be7b4bc87848
Quentin Dequippe <quentin@dequippe.tech>
2025-11-28 10:48:36 +0100

    tranform boolean in string form data

--- src/Component/OpenApi3/Generator/RequestBodyContent/FormBodyContentGenerator.php
+++ src/Component/OpenApi3/Generator/RequestBodyContent/FormBodyContentGenerator.php
@@ -34,6 +34,17 @@ class FormBodyContentGenerator extends AbstractBodyContentGenerator
                                 new Expr\Variable('value')
                             )
                         )),
+                        new Stmt\Expression(new Expr\Assign(new Expr\Variable('value'),
+                            new Expr\Ternary(
+                                new Expr\FuncCall(new Name('is_bool'), [new Arg(new Expr\Variable('value'))]),
+                                new Expr\Ternary(
+                                    new Expr\Variable('value'),
+                                    new Scalar\String_('true'),
+                                    new Scalar\String_('false'),
+                                ),
+                                new Expr\Variable('value')
+                            )
+                        )),
                         new Stmt\If_(
                             new Expr\FuncCall(new Name('is_array'), [new Arg(new Expr\Variable('value'))]),
                             [
