de30713369eca050082802489d3be87d0562f41b
Baptiste Leduc <baptiste.leduc@gmail.com>
2025-12-13 22:48:38 +0100

    Fix #889 - No double encoding for query parameters

--- src/Bundle/OpenApiBundle/Tests/Resources/config/reference.php
+++ src/Bundle/OpenApiBundle/Tests/Resources/config/reference.php
@@ -467,7 +467,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *         max_host_connections?: int, // The maximum number of connections to a single host.
  *         default_options?: array{
  *             headers?: array<string, mixed>,
- *             vars?: list<mixed>,
+ *             vars?: array<string, mixed>,
  *             max_redirects?: int, // The maximum number of redirects to follow.
  *             http_version?: scalar|null, // The default HTTP version, typically 1.1 or 2.0, leave to null for the best version.
  *             resolve?: array<string, scalar|null>,
@@ -490,7 +490,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *                 md5?: mixed,
  *             },
  *             crypto_method?: scalar|null, // The minimum version of TLS to accept; must be one of STREAM_CRYPTO_METHOD_TLSv*_CLIENT constants.
- *             extra?: list<mixed>,
+ *             extra?: array<string, mixed>,
  *             rate_limiter?: scalar|null, // Rate limiter name to use for throttling requests. // Default: null
  *             caching?: bool|array{ // Caching configuration.
  *                 enabled?: bool, // Default: false
@@ -543,7 +543,7 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
  *                 md5?: mixed,
  *             },
  *             crypto_method?: scalar|null, // The minimum version of TLS to accept; must be one of STREAM_CRYPTO_METHOD_TLSv*_CLIENT constants.
- *             extra?: list<mixed>,
+ *             extra?: array<string, mixed>,
  *             rate_limiter?: scalar|null, // Rate limiter name to use for throttling requests. // Default: null
  *             caching?: bool|array{ // Caching configuration.
  *                 enabled?: bool, // Default: false
--- src/Component/OpenApiCommon/Generator/Runtime/data/Client/BaseEndpoint.php
+++ src/Component/OpenApiCommon/Generator/Runtime/data/Client/BaseEndpoint.php
@@ -35,13 +35,16 @@ abstract class BaseEndpoint implements Endpoint
         }, $optionsResolved);
 
         $allowReserved = $this->getQueryAllowReserved();
+        $queryParameters = [];
         foreach ($optionsResolved as $key => $value) {
-            if (!in_array($key, $allowReserved, true)) {
-                $optionsResolved[$key] = urlencode($value);
+            if (in_array($key, $allowReserved, true)) {
+                $queryParameters[] = rawurlencode($key) . '=' . $value;
+            } else {
+                $queryParameters[] = rawurlencode($key) . '=' . rawurlencode($value);
             }
         }
 
-        return http_build_query($optionsResolved, '', '&', \PHP_QUERY_RFC3986);
+        return implode('&', $queryParameters);
     }
 
     public function getHeaders(array $baseHeaders = []): array
