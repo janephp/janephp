c29f40b37ec0e9b23dd63699c41df87c9599d49b
Baptiste Leduc <baptiste.leduc@gmail.com>
2025-12-15 14:41:02 +0100

    Fix #889 - No double encoding for query parameters (#890)

--- src/Component/OpenApi2/Tests/fixtures/multi-specs/expected/Api2/Runtime/Client/BaseEndpoint.php
+++ src/Component/OpenApi2/Tests/fixtures/multi-specs/expected/Api2/Runtime/Client/BaseEndpoint.php
@@ -28,12 +28,15 @@ abstract class BaseEndpoint implements Endpoint
             return $value ?? '';
         }, $optionsResolved);
         $allowReserved = $this->getQueryAllowReserved();
+        $queryParameters = [];
         foreach ($optionsResolved as $key => $value) {
-            if (!in_array($key, $allowReserved, true)) {
-                $optionsResolved[$key] = urlencode($value);
+            if (in_array($key, $allowReserved, true)) {
+                $queryParameters[] = rawurlencode($key) . '=' . $value;
+            } else {
+                $queryParameters[] = rawurlencode($key) . '=' . rawurlencode($value);
             }
         }
-        return http_build_query($optionsResolved, '', '&', \PHP_QUERY_RFC3986);
+        return implode('&', $queryParameters);
     }
     public function getHeaders(array $baseHeaders = []): array
     {
