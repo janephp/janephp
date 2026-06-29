6c0698957f5edf3d3af3f37a7098d233dcffdff9
Quentin Dequippe <quentin.dequippe@tiime.fr>
2026-06-29 10:55:45 +0200

    add test, update CHANGELOG

---
+++ src/Component/OpenApi3/Tests/MultipartBooleanRuntimeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Jane\Component\OpenApi3\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Runtime test for generated multipart/form-data endpoint code.
+ *
+ * This test verifies that boolean form parameters are serialized as
+ * "true"/"false" strings in multipart payloads.
+ */
+class MultipartBooleanRuntimeTest extends TestCase
+{
+    private const FIXTURE_DIR = __DIR__ . '/fixtures/issue-737';
+
+    public function testMultipartBooleanValuesAreEncodedAsStringLiterals(): void
+    {
+        $expectedDir = self::FIXTURE_DIR . '/expected';
+
+        require_once $expectedDir . '/Model/FilePostBody.php';
+        require_once $expectedDir . '/Normalizer/FilePostBodyNormalizer.php';
+        require_once $expectedDir . '/Normalizer/JaneObjectNormalizer.php';
+        require_once $expectedDir . '/Validator/FilePostBodyConstraint.php';
+        require_once $expectedDir . '/Endpoint/PostFile.php';
+
+        $normalizers = [
+            new \Symfony\Component\Serializer\Normalizer\ArrayDenormalizer(),
+            new Expected\Normalizer\JaneObjectNormalizer(),
+        ];
+        $encoders = [
+            new \Symfony\Component\Serializer\Encoder\JsonEncoder(
+                new \Symfony\Component\Serializer\Encoder\JsonEncode(),
+                new \Symfony\Component\Serializer\Encoder\JsonDecode(['json_decode_associative' => true])
+            ),
+        ];
+        $serializer = new \Symfony\Component\Serializer\Serializer($normalizers, $encoders);
+        $streamFactory = \Http\Discovery\Psr17FactoryDiscovery::findStreamFactory();
+
+        $trueBody = new Expected\Model\FilePostBody();
+        $trueBody->setFichier('file-content');
+        $trueBody->setValid(true);
+        $trueResult = (new Expected\Endpoint\PostFile($trueBody))->getBody($serializer, $streamFactory);
+        $trueStreamContent = (string) $trueResult[1];
+        $this->assertMatchesRegularExpression('/name="valid".*?\R\Rtrue\R/s', $trueStreamContent);
+
+        $falseBody = new Expected\Model\FilePostBody();
+        $falseBody->setFichier('file-content');
+        $falseBody->setValid(false);
+        $falseResult = (new Expected\Endpoint\PostFile($falseBody))->getBody($serializer, $streamFactory);
+        $falseStreamContent = (string) $falseResult[1];
+        $this->assertMatchesRegularExpression('/name="valid".*?\R\Rfalse\R/s', $falseStreamContent);
+    }
+}
